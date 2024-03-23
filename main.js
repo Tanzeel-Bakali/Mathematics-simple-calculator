2fb2ddb5787f3ce98508e490bef790ee53ee4e74
TANZEEL-BAKALI <tanzeelbakali@gmail.com>
2024-03-24 03:50:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter Your First Number", type: "number", name: "firstNumber" },
+    {
+        message: "Enter Your SecondNumber",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "select any three of the following operators to perform an operation",
+        type: "list",
+        name: "Operator",
+        choices: ["Addition", "Subtraction", "multiplication", "devision"],
+    },
+]);
+if (answer.Operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.Operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.Operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.Operator === "devision") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
